bb98cc43342a8676451242e59f054b12d6b07310
paulinatrillo <paulina.trillo@hotmail.com>
2025-05-19 22:30:29 -0300

    cambios

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 const router = Router();
 
 router.post('/login', (req, res, next) => {
-  passport.authenticate('login', (err, user, info) => {
+  passport.authenticate('login', { session: false }, (err, user, info) => {
     if (err) return next(err);
     if (!user) return res.status(401).json({ message: 'Credenciales inválidas' });
 
